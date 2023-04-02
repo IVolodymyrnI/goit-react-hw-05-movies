9c126664bb3c5716f8e2d13bb7d6f371dc456b3c
Volodymyr Yarchuk <volodia.iarcguk.forwork@gmail.com>
2023-04-02 09:55:46 +0300

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,7 +6,7 @@ export function NotFoundPage() {
       <h1>Error 404</h1>
       <p>{`It seems this page doesn't exist =(`}</p>
       <p>
-        You might try and search{' '}
+        You might try and search
         <Link to="/">
           <b>this page</b>
         </Link>
